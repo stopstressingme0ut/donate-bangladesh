7a167edaa88c040ede730bbff79135424a3e0f54
Rafi <rafiahmedgp@gmail.com>
2024-09-25 02:10:31 +0600

    updated js

--- js/utilities.js
+++ js/utilities.js
@@ -14,8 +14,26 @@ function calculate(Amount,myBalance,locationBalance,id,locationTitle){
     }else{
         modal1.showModal();
         document.getElementById(id).innerText = (locationBalance + Amount);
-        document.getElementById('balance').innerText = (myBalance-Amount);
-        history(Amount,locationTitle);
+        document.getElementById('balance').innerText = (myBalance - Amount);
+        historyFunc(Amount,locationTitle);
     }
 }
+
+function historyFunc(Amount,locationTitle){
+    const div = document.createElement('div');
+    const time = new Date()
+    const dateTime = time.toString();
+    div.classList.add( 'flex', 'flex-col', 'gap-2', 'p-8', 'border-2', 'rounded-xl');
+    div.innerHTML = `
+        <h2 class="text-lg font-bold text-black">
+            ${Amount} Taka donated to: ${locationTitle}
+        </h2>
+        <p class="text-[#111111B3] rounded-xl px-2">Date : 
+            ${dateTime}
+        </p>   
+    `;
+    document.getElementById('historySec').appendChild(div);
+   
+}
+
 
